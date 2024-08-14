9e9778282e329ed952ed3496a8d83139f5ea75bd
shaikanwar5304 <shaikanwar5304@gmail.com>
2024-08-14 23:37:14 +0530

    improved carousel functionality

--- CodingShuttle/JS/js8_fetch_DomRevisit/C_Carousel/script.js
+++ CodingShuttle/JS/js8_fetch_DomRevisit/C_Carousel/script.js
@@ -1,38 +1,53 @@
-imgarr = [
-    "https://dummyjson.com/image/200x100/008080/ffffff?text=1+image",
-    "https://dummyjson.com/image/400x200/008080/ffffff?text=2+image",
-    "https://dummyjson.com/image/600x300/008080/ffffff?text=3+image",
-    "https://dummyjson.com/image/800x400/008080/ffffff?text=4+image",
-];
-img=[]
-carousel = document.getElementsByClassName("carouselimages")[0]
-for(i=0;i<imgarr.length;i++){
-    img[i]=document.createElement("img");
-    img[i].src=imgarr[i]
-    carousel.append(img[i])
+img = [];
+dots = [];
+n = 50;
+dotsNav = document.getElementsByClassName("nav")[0];
+dotsNav.style.gridTemplateColumns = `repeat(${n},1fr)`;
+dotsNav.style.gap = `${10 / n + 1}%`;
+
+carousel = document.getElementsByClassName("carouselimages")[0];
+
+for (i = 0; i < n; i++) {
+  img[i] = document.createElement("img");
+  dots[i] = document.createElement("div");
+
+  img[i].src = `https://dummyjson.com/image/1000x500/${Math.round(999999 * Math.random())}/ffffff?text=${i + 1}+image`;
+
+  carousel.append(img[i]);
+  dotsNav.append(dots[i]);
+
+  dots[i].classList.add("dots");
+  dots[i].addEventListener("click", (e) => {
+    currentPos = dots.indexOf(e.target);
+    updateView();
+  });
 }
 
-currentPos=0;
-leftbtn=document.getElementsByClassName("leftarr")[0]
-rightbtn=document.getElementsByClassName("rightarr")[0]
+prevPos = currentPos = 0;
+leftbtn = document.getElementsByClassName("leftarr")[0];
+rightbtn = document.getElementsByClassName("rightarr")[0];
 
-if(imgarr.length==1){
-    rightbtn.style.display = "none";
+if (n == 1) {
+  rightbtn.style.display = "none";
 }
 
-rightbtn.addEventListener('click',(e)=>{
-    currentPos++;
-    if(currentPos==imgarr.length-1){
-        rightbtn.style.display="none"
-    }
-    leftbtn.style.display = "flex";
-    carousel.style.transform=`translateX(${-(currentPos)*100}%)`;
-})
-leftbtn.addEventListener("click", (e) => {
-    currentPos--;
-    if(currentPos==0){
-        leftbtn.style.display="none"
-    }
-    rightbtn.style.display = "flex";
-    carousel.style.transform = `translateX(${-(currentPos) * 100}%)`;
+rightbtn.addEventListener("click", (e) => {
+  currentPos++;
+  updateView();
 });
+leftbtn.addEventListener("click", (e) => {
+  currentPos--;
+  updateView();
+});
+function updateView() {
+  if (currentPos > 0) leftbtn.style.display = "flex";
+  else leftbtn.style.display = "none";
+  if (currentPos < n - 1) rightbtn.style.display = "flex";
+  else rightbtn.style.display = "none";
+
+  carousel.style.transform = `translateX(${-currentPos * 100}%)`;
+  dots[prevPos].classList.remove("active");
+  dots[currentPos].classList.add("active");
+  prevPos = currentPos;
+}
+updateView();
